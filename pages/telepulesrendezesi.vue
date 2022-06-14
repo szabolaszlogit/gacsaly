885f339ef8dabece9096a24b5fc70acc9f327d97
OverrideStudio <szabolaszlo.mail@gmail.com>
2022-06-14 14:33:46 +0200

    telepulesrendezesi2

--- pages/telepulesrendezesi.vue
+++ pages/telepulesrendezesi.vue
@@ -11,7 +11,10 @@
           <b-list-group v-for="doc in docs" :key="doc.desc">
             <b-list-group-item>
               <b-button-group class="mb-3">
-                <b-button variant="success" :href="'/telepulesrendezesi/' + doc.link" download
+                <b-button
+                  variant="success"
+                  :href="'/telepulesrendezesi/' + doc.link"
+                  download
                   >Letöltés</b-button
                 >
                 <b-button
@@ -48,7 +51,11 @@ export default {
         {
           desc: "Gacsály RT módosítás - 2022 I. - ETD - Térségi övezeti",
           link: "gacsaly_rt_modositas_2022_1_etd_tersegi.pdf",
-        }
+        },
+        {
+          desc: "Gacsály RT módosítás - 2022 I. - Véleményezési dokumentáció",
+          link: "gacsaly_rt_modositas_2022_1_velemenyezesi_dokumentacio.pdf",
+        },
       ],
     };
   },
